201962973625226fbf8f8ee996ab7d7be5660975
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2023-11-06 10:09:34 -0300

    add

---
+++ 01 - Testes de Unidade/01 - Testes de Unidade/1.2 - Features/Features.Tests/1-Traits/ClienteTests.cs
@@ -0,0 +1,52 @@
+﻿using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            //Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Eduardo",
+                "Pires",
+                DateTime.Now.AddYears(-30),
+                "edu@gmail.com",
+                true,
+                DateTime.Now);
+
+            //Act
+            bool result = cliente.EhValido();
+
+            //Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            //Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "",
+                false,
+                DateTime.Now);
+
+            //Act
+            bool result = cliente.EhValido();
+
+            //Assert
+            Assert.False(result);
+            Assert.Equal(7, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
